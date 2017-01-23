de92beeb26f18afcedc81a38454b1226cd085882
Dmytro Aleksandrov <alexandrov.dmitry@gmail.com>
2017-01-23 22:33:51 +0200

    call curl for each file

--- src/plugin.sh
+++ src/plugin.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 echo "Searching for ${PLUGIN_FILES}"
+
+find /drone/src -name $PLUGIN_FILES -print0 | xargs -0 echo curl --verbose -X POST $PLUGIN_FEED_URL -H "X-NuGet-ApiKey: ${PLUGIN_API_KEY}" -T 
--- test.sh
+++ test.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+mkdir /tmp/drone
+mkdir /tmp/drone/src
+mkdir /tmp/drone/src/sub
+touch /tmp/drone/src/sub/test.nupkg
+
 docker run --rm \
+  -v /tmp/drone/src:/drone/src \
   -e PLUGIN_FILES=*.nupkg \
-  -e PLUGIN_API_KEY=post \
+  -e PLUGIN_API_KEY=ololololo \
   -e PLUGIN_FEED_URL=https://www.myget.org/F/backend/api/v2/package \
   aleksandrovd/drone-curl-nuget
+
+rm -rf /tmp/drone  
