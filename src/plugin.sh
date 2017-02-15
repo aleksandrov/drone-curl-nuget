7298997ab0ee1e40e182e529318e1bd2d511ad4b
Dmytro Aleksandrov <alexandrov.dmitry@gmail.com>
2017-02-15 17:15:03 +0200

    clean up

--- src/plugin.sh
+++ src/plugin.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-echo "Searching for ${PLUGIN_FILES}"
+env
 
-find /drone/src -name $PLUGIN_FILES -print0 | xargs -0 -I{} curl -X POST "${PLUGIN_FEED_URL}" -H "X-NuGet-ApiKey: ${PLUGIN_API_KEY}" -T {}
+if [ -z "$PLUGIN_API_KEY" ]; then 
+	echo "No Nuget API key provided" 
+	exit 1
+else 
+	echo "Searching for ${PLUGIN_FILES}"
+	find /drone/src -name $PLUGIN_FILES -print0 | xargs -0 -I{} curl -s -X POST "${PLUGIN_FEED_URL}" -H "X-NuGet-ApiKey: ${PLUGIN_API_KEY}" -T {}
+fi
+
--- test.sh
+++ test.sh
@@ -9,8 +9,8 @@ touch /tmp/drone/src/sub/test2.nupkg
 docker run --rm \
   -v /tmp/drone/src:/drone/src \
   -e PLUGIN_FILES=*.nupkg \
-  -e PLUGIN_API_KEY=ololololo \
-  -e PLUGIN_FEED_URL=https://www.myget.org/F/backend/api/v2/package \
+  -e PLUGIN_API_KEY=$API_KEY \
+  -e PLUGIN_FEED_URL=$FEED_URL \
   aleksandrovd/drone-curl-nuget
 
 rm -rf /tmp/drone  
